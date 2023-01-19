c31b2307251b25c156106de2af1cbd5ac7b72bb4
Pinghui Tan <w0753597@myscc.ca>
2023-01-18 21:04:04 -0500

    Added a constructor and some methods to ContentList class

--- src/app/models/content-list.ts
+++ src/app/models/content-list.ts
@@ -1,2 +1,21 @@
+import { IContent } from "./icontent";
+
 export class ContentList {
+    private _contents: IContent[];
+
+    public constructor(contents: IContent[]) {
+        this._contents = contents;
+    }
+
+    public getContents(): IContent[] {
+        return this._contents;
+    }
+
+    public addContent(content: IContent) {
+        this._contents.push(content);
+    }
+
+    public length(): number {
+        return this._contents.length;
+    }
 }
