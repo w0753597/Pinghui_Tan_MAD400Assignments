5e3c830849ec0e9f3f7c05640ea31904bb3b3def
Pinghui Tan <w0753597@myscc.ca>
2023-01-18 20:49:05 -0500

    Added ContentList class

---
+++ src/app/models/content-list.spec.ts
@@ -0,0 +1,7 @@
+import { ContentList } from './content-list';
+
+describe('ContentList', () => {
+  it('should create an instance', () => {
+    expect(new ContentList()).toBeTruthy();
+  });
+});
---
+++ src/app/models/content-list.ts
@@ -0,0 +1,2 @@
+export class ContentList {
+}
