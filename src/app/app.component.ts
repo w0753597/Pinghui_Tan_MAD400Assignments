2352d0b61f085c43133ea04163d8cc87df859166
Pinghui Tan <w0753597@myscc.ca>
2023-01-18 22:49:00 -0500

    Built a test ContentList object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { isNgContent } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { ContentList } from './models/content-list';
+import { IContent } from './models/icontent';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'P_Tan_DroneDIY';
+  title = 'DIY a drone';
+  list = this.buildSampleList();
+
+  private buildSampleList(): ContentList {
+    let list = new ContentList();
+
+    let content: IContent = {
+      id: 1, 
+      name: "On Everything",
+      author: "Weed John",
+      type: "Religion"
+    };    
+    list.addContent(content);
+
+    content = {
+      id: 2, 
+      name: "When Sam Meet Eva",
+      author: "Doe Indie",
+      type: "Romance"
+    };    
+    list.addContent(content);
+
+    content = {
+      id: 3, 
+      name: "What You Think Is Not What You Get",
+      author: "Rasha Willimson",
+      type: "Philosophy"
+    };    
+    list.addContent(content);
+
+    content = {
+      id: 4, 
+      name: "Nonexistence",
+      author: "Xenello Zewa",
+      type: "Religion"
+    };    
+    list.addContent(content);
+    
+    return list;
+  }
 }
