02939d81cce438a7c5d8638e2f5b0908782e82e4
Pinghui Tan <w0753597@myscc.ca>
2023-01-18 21:48:36 -0500

    added toString method to class ContentList

--- src/app/models/content-list.ts
+++ src/app/models/content-list.ts
@@ -18,4 +18,17 @@ export class ContentList {
     public length(): number {
         return this._contents.length;
     }
+
+    public toString(index: number): string {
+        let content = this._contents[index];
+        let result = "";
+        result += "<p class='content-id'>" + content.id + "</p>";
+        result += "<p class='content-name'>" + content.name + "</p>";
+        result += "<p class='content-desc'>" + content.description + "</p>";
+        result += "<p class='content-author'>" + content.author + "</p>";
+        result += "<img class='content-imgsrc' src='" + content.imgSrc + "'/>";
+        result += "<p class='content-type'>" + content.type + "</p>";
+        result += "<p class='content-tags'>" + content.tags + "</p>";
+        return result;
+    }
 }
