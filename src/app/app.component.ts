88ccf27f9e05115d42e03ff1f327b7582f3cbb4a
Pinghui Tan <w0753597@myscc.ca>
2023-01-24 20:18:08 -0500

    version => 0.2.0 delete ContentList class and its references

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { isNgContent } from '@angular/compiler';
 import { Component } from '@angular/core';
-import { ContentList } from './models/content-list';
 import { IContent } from './models/icontent';
 
 @Component({
@@ -10,43 +9,4 @@ import { IContent } from './models/icontent';
 })
 export class AppComponent {
   title = 'DIY a drone';
-  list = this.buildSampleList();
-
-  private buildSampleList(): ContentList {
-    let list = new ContentList();
-
-    let content: IContent = {
-      id: 1, 
-      name: "On Everything",
-      author: "Weed John",
-      type: "Religion"
-    };    
-    list.addContent(content);
-
-    content = {
-      id: 2, 
-      name: "When Sam Meet Eva",
-      author: "Doe Indie",
-      type: "Romance"
-    };    
-    list.addContent(content);
-
-    content = {
-      id: 3, 
-      name: "What You Think Is Not What You Get",
-      author: "Rasha Willimson",
-      type: "Philosophy"
-    };    
-    list.addContent(content);
-
-    content = {
-      id: 4, 
-      name: "Nonexistence",
-      author: "Xenello Zewa",
-      type: "Religion"
-    };    
-    list.addContent(content);
-    
-    return list;
-  }
 }
--- src/app/models/content-list.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ContentList } from './content-list';
-
-describe('ContentList', () => {
-  it('should create an instance', () => {
-    expect(new ContentList()).toBeTruthy();
-  });
-});
