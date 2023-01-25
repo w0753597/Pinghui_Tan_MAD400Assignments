1c80a7002df160997f650198a7d3368f75f28738
Pinghui Tan <w0753597@myscc.ca>
2023-01-24 22:32:14 -0500

    Added <app-content-list> tag to  html Added an IContent array

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ɵisListLikeIterable } from '@angular/core';
+import { IContent } from '../models/icontent';
 
 @Component({
   selector: 'app-content-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-list.component.scss']
 })
 export class ContentListComponent {
-
+  contentList: IContent[] = [
+    { id: 1, name: "On Everything", author: "Weed John", type: "Religon" },
+    { id: 2, name: "When Sam Meet Eva", author: "Doe Indie", type: "Romance" },
+    { id: 3, name: "What You Think Is Not What You Get", author: "Rasha Willimson", type: "Philosophy" },
+    { id: 4, name: "Nonexistence", author: "Xenello Zewa", type: "Religion" }
+  ];
 }
