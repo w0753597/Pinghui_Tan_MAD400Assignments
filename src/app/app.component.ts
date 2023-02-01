255d2e1f6744a815bc76813164c288b1ee27c5ba
Pinghui Tan <w0753597@myscc.ca>
2023-01-31 23:35:42 -0500

    Step 10-13, added an item with box-shadow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { isNgContent } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { IContent } from './models/icontent';
+import { DronePartService } from './services/drone-part.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,12 @@ import { IContent } from './models/icontent';
 })
 export class AppComponent {
   title = 'DIY a drone';
+  part?: IContent 
+
+  constructor(private dronePartService: DronePartService) { }
+
+  ngOnInit() {
+    this.dronePartService.getDronePart(2).subscribe(p => this.part = p);
+  }
+
 }
