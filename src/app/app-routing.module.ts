6e22337b4ecbe759fd41134d6a244f20c6eae7ed
Pinghui Tan <w0753597@myscc.ca>
2023-02-11 10:40:10 -0500

    Finished Step 7: added routes "/content', "/", "/content/:id" and made them work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes} from '@angular/router';
+import { ContentListComponent } from './content-list/content-list.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
 
-
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "content",
+    pathMatch: "full",
+  },
+  {
+    path: "content",
+    component: ContentListComponent,
+  },
+  {
+    path: "content/:id",
+    component: ItemDetailComponent,
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
--- src/app/item-detail/item-detail.component.ts
+++ src/app/item-detail/item-detail.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IContent } from '../models/icontent';
+import { DronePartService } from '../services/drone-part.service';
 
 @Component({
   selector: 'app-item-detail',
   templateUrl: './item-detail.component.html',
   styleUrls: ['./item-detail.component.scss']
 })
-export class ItemDetailComponent {
+export class ItemDetailComponent implements OnInit {
+  dronePart?: IContent;
+
+  constructor(
+    private router: Router,
+    private dronePartService: DronePartService,
+    private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      let id: number = 0;
+      id = +(params.get('id') ?? 1024);
+
+      this.dronePartService.getDronePart(id).subscribe((dronePart: IContent) => {
+        if (dronePart) {
+          this.dronePart = dronePart;
+        }
+        else {
+//          this.router.navigate(["/contentNotFoundHere"]);
+        }
+      });
+
+    });
+  }
 
 }
