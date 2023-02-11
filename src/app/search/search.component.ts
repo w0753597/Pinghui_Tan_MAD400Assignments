df805f8a9fe1ec0e23c42691744402968ac85180
Pinghui Tan <w0753597@myscc.ca>
2023-02-11 11:30:24 -0500

    Steps 10, 11, 12 finished.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IContent } from '../models/icontent';
+import { DronePartService } from '../services/drone-part.service';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent {
+  part?: IContent 
+
+  constructor(private dronePartService: DronePartService) { }
+
+  ngOnInit() {
+    this.dronePartService.getDronePart(2).subscribe(p => this.part = p);
+  }
+
+  showPart(id: string) {
+    let value = (document.getElementById(id) as HTMLInputElement).value
+    this.dronePartService.getDronePart(+value).subscribe(p => this.part = p);
+  }
 
 }
