f56f752bce777b31366db94dd48379c622988633
Pinghui Tan <w0753597@myscc.ca>
2023-03-24 12:35:42 -0400

    Step 11: Added ModifyContent component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './services/in-memory-data.service';
+import { ModifyContentComponent } from './modify-content/modify-content.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { InMemoryDataService } from './services/in-memory-data.service';
     ItemDetailComponent,
     SearchComponent,
     UrlErrorComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    ModifyContentComponent
   ],
   imports: [
     BrowserModule,
