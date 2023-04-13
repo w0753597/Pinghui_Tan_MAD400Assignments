1056b95be9bb6c481b083fc5409b9fbe22e1445d
Pinghui Tan <w0753597@myscc.ca>
2023-04-13 13:53:10 -0400

    Assignment 6b Change individial tag text to another color when mouse over Restore original color when mouse out

--- src/app/directives/interactive-hightlight.directive.ts
+++ src/app/directives/interactive-hightlight.directive.ts
@@ -6,22 +6,28 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 export class InteractiveHightlightDirective {
 
   private savedBorder: string;
+  private savedColor: string;
   @Input() from?: string;
   @Input() colour?: string;
 
   constructor(private elm: ElementRef) { 
     this.savedBorder = elm.nativeElement.style.border;
+    this.savedColor = elm.nativeElement.style.color;
   }
   
   @HostListener('mouseover') public onMouseOver() {
     if (this.from == "type") {
       this.elm.nativeElement.style.border = "thick dotted #0000FF";
+    } else if (this.from == 'tag') {
+      this.elm.nativeElement.style.color = "white";
     }
   }
 
   @HostListener('mouseout') public onMouseOut() {
     if (this.from == "type") {
       this.elm.nativeElement.style.border = this.savedBorder;
+    } else if (this.from == 'tag') {
+      this.elm.nativeElement.style.color = this.savedColor;
     }
   }
 
