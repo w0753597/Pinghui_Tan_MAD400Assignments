52ac57b225180ed9f0cba03ee5f6e1b9ba5c76f5
Pinghui Tan <w0753597@myscc.ca>
2023-04-13 13:11:17 -0400

    Assignment 6b add border around type when mouse over it restore original border when mouse out

--- src/app/directives/interactive-hightlight.directive.ts
+++ src/app/directives/interactive-hightlight.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appInteractiveHightlight]'
 })
 export class InteractiveHightlightDirective {
 
-  constructor() { }
+  private savedBorder: string;
+  @Input() from?: string;
 
+  constructor(private elm: ElementRef) { 
+    this.savedBorder = elm.nativeElement.style.border;
+  }
+  
+  @HostListener('mouseover') public onMouseOver() {
+    if (this.from == "type") {
+      this.elm.nativeElement.style.border = "thick dotted #0000FF";
+    }
+  }
+
+  @HostListener('mouseout') public onMouseOut() {
+    if (this.from == "type") {
+      this.elm.nativeElement.style.border = this.savedBorder;
+    }
+  }
 }
